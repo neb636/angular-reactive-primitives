d8ae0e3bb921699dd46cccb38691f79ce884d49f
Nick <neb636@gmail.com>
2025-10-30 18:47:32 -0400

    Changes

---
+++ projects/documentation-site/src/app/common/effects/scroll-to-fragment-on-update/scroll-to-fragment-on-update.effect.ts
@@ -0,0 +1,14 @@
+import { effect } from '@angular/core';
+import { useRouteFragment } from '../../../../../../reactive-primitives/src/lib/composables/route/use-route-fragment/use-route-fragment.composable';
+
+export const scrollToFragmentOnUpdateEffect = () => {
+  const routeFragment = useRouteFragment();
+
+  return effect(() => {
+    const fragment = routeFragment();
+
+    if (fragment) {
+      document.getElementById(fragment)?.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+};
